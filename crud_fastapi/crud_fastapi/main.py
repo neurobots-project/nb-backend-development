0868f1ea24263122b95b7bbe2774329e93450022
Lohhan <lohhang05@gmail.com>
2024-05-03 18:47:12 -0300

    Arquivos atualizados (Firestore)

--- crud_fastapi/crud_fastapi/main.py
+++ crud_fastapi/crud_fastapi/main.py
@@ -1,57 +1,63 @@
 from typing import List
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
-import requests
-import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("crud_fastapi/secret.json")
+firebase_admin.initialize_app(cred)
 
 app = FastAPI()
 
-linkDB = "https://residenciadb-7c712-default-rtdb.firebaseio.com/"
+db = firestore.client()
 
 class Paciente(BaseModel):
     nome: str
+    email: str
     telefone: str
     cep: str
     endereco: str
     cidade: str
     estado: str
 
-# Rota para criar um paciente
+# Rota para criar um paciente (CREATE)
 @app.post("/pacientes/", status_code=status.HTTP_201_CREATED)
 def criar_paciente(paciente: Paciente):
-    request = requests.post(f"{linkDB}/Pacientes/.json", data=json.dumps(paciente.model_dump()))
-    if request.status_code != 200:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao criar paciente")
-    return {"mensagem": "Paciente criado com sucesso"}
+    doc_ref = db.collection("pacientes").add(paciente.model_dump())
+    return {"mensagem": "Paciente criado com sucesso", "id": doc_ref.id}
 
-# Rota para obter todos os pacientes
+# Rota para obter todos os pacientes (GET)
 @app.get("/pacientes/", response_model=List[Paciente])
 def obter_pacientes():
-    request = requests.get(f"{linkDB}/Pacientes/.json")
-    if request.status_code != 200:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao obter pacientes")
-    return [Paciente(**dados) for dados in request.json().values()]
+    pacientes = []
+    docs = db.collection("pacientes").stream()
+    for doc in docs:
+        paciente_data = doc.to_dict()
+        pacientes.append(Paciente(**paciente_data))
+    return pacientes
 
-# Rota para obter um paciente por ID
+# Rota para obter um paciente por ID (GET)
 @app.get("/pacientes/{paciente_id}", response_model=Paciente)
 def obter_paciente(paciente_id: str):
-    request = requests.get(f"{linkDB}/Pacientes/{paciente_id}/.json")
-    if request.status_code != 200:
+    doc_ref = db.collection("pacientes").document(paciente_id)
+    doc = doc_ref.get()
+    if not doc.exists:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Paciente não encontrado")
-    return Paciente(**request.json())
+    paciente_data = doc.to_dict()
+    return Paciente(**paciente_data)
 
-# Rota para atualizar um paciente por ID
+# Rota para atualizar um paciente por ID (UPDATE)
 @app.put("/pacientes/{paciente_id}", response_model=Paciente)
 def atualizar_paciente(paciente_id: str, paciente: Paciente):
-    request = requests.put(f"{linkDB}/Pacientes/{paciente_id}/.json", data=json.dumps(paciente.model_dump()))
-    if request.status_code != 200:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao atualizar paciente")
+    doc_ref = db.collection("pacientes").document(paciente_id)
+    doc_ref.set(paciente.model_dump())
     return Paciente(**paciente.model_dump(), id=paciente_id)
 
-# Rota para deletar um paciente por ID
+# Rota para deletar um paciente por ID (DELETE)
 @app.delete("/pacientes/{paciente_id}")
 def deletar_paciente(paciente_id: str):
-    request = requests.delete(f"{linkDB}/Pacientes/{paciente_id}/.json")
-    if request.status_code != 200:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao deletar paciente")
+    doc_ref = db.collection("pacientes").document(paciente_id)
+    doc_ref.delete()
     return {"mensagem": "Paciente deletado com sucesso"}
+
+# uvicorn crud_fastapi.main:app --reload
